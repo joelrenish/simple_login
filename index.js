1ea3887049e6feec4cf8509830f14a9cacf3dcbc
joelrenish <joelrenish@gmail.com>
2023-06-30 12:30:48 +0530

    added picture

--- index.js
+++ index.js
@@ -34,21 +34,30 @@ document.getElementById("password").addEventListener("input", (event) => {
   event.target.classList.remove("failed");
 });
 
+// disallow e from being entered
+document.getElementById("phone").addEventListener("keydown", function(e) {
+  if (["-", "e"].includes(e.key)) {
+    e.preventDefault();
+  }
+});
+
 // change forms with these buttons
 document.getElementById("otpButton").addEventListener("click", (event) => {
   event.target.classList.add("selected");
   document.getElementById("regularButton").classList.remove("selected");
-
   document.getElementById("loginForm").classList.add("hidden");
   document.getElementById("OTPForm").classList.remove("hidden");
+
+  document.getElementById("successMessage").classList.add("hidden");
 });
 
 document.getElementById("regularButton").addEventListener("click", (event) => {
   event.target.classList.add("selected");
   document.getElementById("otpButton").classList.remove("selected");
-
   document.getElementById("OTPForm").classList.add("hidden");
   document.getElementById("loginForm").classList.remove("hidden");
+
+  document.getElementById("successMessage").classList.add("hidden");
 });
 
 function login(username, password) {
@@ -61,4 +70,12 @@ function login(username, password) {
   console.log(`Logged in as ${username.value} using ${password.value} as the password`);
 
   localStorage.setItem('username', username.value);
+  localStorage.setItem('pw', password.value);
 }
+
+document.getElementById("OTPForm").addEventListener("submit", (event) => {
+  event.preventDefault();
+  successMessage.classList.remove("hidden");
+  successMessage.textContent = `OTP Sent`;
+  event.target.lastElementChild.value = 'Resend OTP';
+});
