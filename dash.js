cd16f03c3b46d9023d5c3cae790700660fa3cca8
joelrenish <joelrenish@gmail.com>
2023-07-06 15:38:06 +0530

    bad UI dashboard

--- dash.js
+++ dash.js
@@ -1,8 +1,22 @@
-document.getElementById("navLogin").addEventListener("click", (event) => {
-  if(localStorage.getItem("username") != null) {
-    console.log("bruh");
-    goToDash();
-  } else {
+document.getElementById("navLogin").addEventListener("click", () => {
+  if(localStorage.getItem("username") === null) {
     document.location.href="index.html";
+  }
+});
+
+document.getElementById("login").addEventListener("click", () => {
+
+  if(localStorage.getItem("username") === null) {
+    document.location.href="index.html";
+  }
+});
+
+document.getElementById("menuButton").addEventListener("click", () => {
+  document.querySelector(".nav-container").classList.toggle("navclose");
+  document.querySelector(".main").classList.toggle("mainclose");
+  let navOptions = document.querySelector(".nav-container").firstElementChild.children;
+  for (let i = 0; i < navOptions.length; i++) {
+    navOptions[i].classList.toggle("navclose");
+    navOptions[i].lastElementChild.classList.toggle("hidden");
   }
 });
